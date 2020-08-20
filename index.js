62d7ba945452aeab85e46bb6216f7078b9449070
Tyler Jacobson <TylerJacobsonSE@gmail.com>
2020-08-20 12:42:04 -0700

    Initialized Person with parameters

--- index.js
+++ index.js
@@ -41,6 +41,15 @@ class Airplane {
 */
 
 class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+
+  eat("someFood") {
+    
+  }
 
 }
 
